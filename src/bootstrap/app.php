1de50aa5b3119659448fe020b6bfc768950acaf3
sekirubeg <niseyanagitetsuko@gmail.com>
2025-04-24 00:48:39 +0900

    dev-env

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Providers\FortifyServiceProvider;
+use App\Providers\RouteServiceProvider;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,4 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withProviders([
+        FortifyServiceProvider::class,
+        RouteServiceProvider::class,
+    ])->create();
